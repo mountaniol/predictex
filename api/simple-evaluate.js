75cc60b1eb00a9fd388a295c354682e0ac96e39a
Sebastian Mountaniol <mountaniol@gmail.com>
2025-08-08 19:50:57 +0200

    Fix ES modules for Vercel API functions

--- api/simple-evaluate.js
+++ api/simple-evaluate.js
@@ -1,6 +1,6 @@
-const axios = require('axios');
+import axios from 'axios';
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
